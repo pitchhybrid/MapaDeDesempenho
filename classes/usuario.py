de14a315c3012411e49a4931c1f2cd82f29646bd
pitchhybrid <messias_junior@outlook.com>
2019-06-05 01:32:30 -0300

    2018-06-05

--- classes/usuario.py
+++ classes/usuario.py
@@ -3,7 +3,7 @@ from os import path
 from hashlib import md5
 
 class Usuario(object):
-    conn = sql.connect(path.abspath("handymobile.db"),check_same_thread=False)
+    conn = sql.connect(path.abspath("handymobile.db"),timeout=500,check_same_thread=False)
     
     #constutor
     def __init__(self):
@@ -11,55 +11,66 @@ class Usuario(object):
 
     def addUsuario(self,login,senha,nome,email):
         temp = md5(senha.encode()).hexdigest()
-        c = self.conn.cursor()
-        c.execute("INSERT INTO mapa_users (login,password,nome,email) VALUES (?,?,?,?)",(login,temp,nome,email))
+        with self.conn:
+            c = self.conn.cursor()
+            c.execute("INSERT INTO mapa_users (login,password,nome,email) VALUES (?,?,?,?)",(login,temp,nome,email))
 
     def remUsuario(self,usuario_id):
-        c = self.conn.cursor()
-        c.execute("DELETE FROM mapa_users where id=" + str(usuario_id))
+        with self.conn:
+            c = self.conn.cursor()
+            c.execute("DELETE FROM mapa_users where id=" + str(usuario_id))
 
     def editaUsuario(self,login,senha,nome,email,usuario_id):
         temp = md5(senha.encode()).hexdigest()
-        c = self.conn.cursor()
-        c.execute("UPDATE mapa_users SET login='"+ login +"', password='"+ temp +"',nome='" + nome + "',email='"+ email +"' WHERE id=" + str(usuario_id))
+        with self.conn:
+            c = self.conn.cursor()
+            c.execute("UPDATE mapa_users SET login='"+ login +"', password='"+ temp +"',nome='" + nome + "',email='"+ email +"' WHERE id=" + str(usuario_id))
     
     def login(self,login,senha):
         temp = md5(senha.encode()).hexdigest()
-        c = self.conn.cursor()
-        c.execute("SELECT login,password FROM mapa_users WHERE login='"+ login + "' AND password='" + temp + "'")
-        return c.fetchone()
+        with self.conn:
+            c = self.conn.cursor()
+            c.execute("SELECT login,password FROM mapa_users WHERE login='"+ login + "' AND password='" + temp + "'")
+            return c.fetchone()
 
     def getUsuarios(self):
-        c = self.conn.cursor()
-        c.execute("SELECT * FROM mapa_users")
-        return c.fetchall()
+        with self.conn:
+            c = self.conn.cursor()
+            c.execute("SELECT * FROM mapa_users")
+            return c.fetchall()
 
     def getUsuario(self,user_id):
-        c = self.conn.cursor()
-        c.execute("SELECT * FROM mapa_users WHERE login='" + str(user_id) + "'")
-        return c.fetchone()
+        with self.conn:
+            c = self.conn.cursor()
+            c.execute("SELECT * FROM mapa_users WHERE login='" + str(user_id) + "'")
+            return c.fetchone()
     
     def getFuncionarios(self):
-        c = self.conn.cursor()
-        c.execute("SELECT * FROM mapa_funcionarios")
-        return c.fetchall()
+        with self.conn:
+            c = self.conn.cursor()
+            c.execute("SELECT * FROM mapa_funcionarios")
+            return c.fetchall()
 
     def getFuncionario(self,codfun):
-        c = self.conn.cursor()
-        c.execute("SELECT * FROM mapa_funcionarios where cod="+ str(codfun) )
-        return c.fetchall()
+        with self.conn:
+            c = self.conn.cursor()
+            c.execute("SELECT * FROM mapa_funcionarios where cod="+ str(codfun) )
+            return c.fetchall()
    
     def addFuncionario(self,codfun,nomefun):
-        c = self.conn.cursor()
-        c.execute("INSERT INTO mapa_funcionarios (cod,funcionario) VALUES (?,?)",(codfun,nomefun))
+        with self.conn:
+            c = self.conn.cursor()
+            c.execute("INSERT INTO mapa_funcionarios (cod,funcionario) VALUES (?,?)",(codfun,nomefun))
 
     def remFuncionario(self,codfun):
-        c = self.conn.cursor()
-        c.execute("DELETE FROM mapa_funcionarios where cod=" + str(codfun))
+        with self.conn:
+            c = self.conn.cursor()
+            c.execute("DELETE FROM mapa_funcionarios where cod=?",(codfun))
 
     def editaFuncionario(self,codfun,nomefun):
-        c = self.conn.cursor()
-        c.execute("UPDATE mapa_funcionarios SET funcionario='"+ nomefun +"' WHERE cod=" + str(codfun))
+        with self.conn:
+            c = self.conn.cursor()
+            c.execute("UPDATE mapa_funcionarios SET funcionario='"+ nomefun +"' WHERE cod=" + str(codfun))
 
 # def definirTempoMin(self):
 #     pass
